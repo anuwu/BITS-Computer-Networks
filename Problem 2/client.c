fbb64f201c682bad856b28c2130344eedfd544c3
anwesh0304 <anwesh0304@gmail.com>
2020-04-28 04:20:11 +0530

    Moving to lab branch to bring back upd server/client.

--- Problem 2/client.c
+++ Problem 2/client.c
@@ -55,38 +55,52 @@ int unsentInWindow (int *windowPktStat)
     return 0 ;
 }
 
+void initWindow (int *windowPktOffset, int *windowPktStat)
+{
+    for (int i = 0 ; i < WINDOW_SIZE ; i++)
+    {
+        windowPktOffset[i] = -1 ;
+        windowPktStat[i] = 0 ;
+    }
+}
+
 int main(void)
 {
 	struct sockaddr_in relayEvenAddr, relayOddAddr, otherAddr ;
     int relayEvenSock, relayOddSock, i, slen = sizeof(struct sockaddr_in) ;
     int sndCount, fileSize, noPkts, bytesRead ;
     struct timeval timeout = {TIMEOUT,0} ;
-
     data *datPkt, *ackPkt ;
 	data *datCache [WINDOW_SIZE] ;
 
-	FILE *fp = fopen ("input.txt", "r") ;
+    /* ------------------------------------------------------ */
 
+	FILE *fp = fopen ("input.txt", "r") ;
 	fseek (fp, 0, SEEK_END) ;
 	fileSize = ftell (fp)  ;
 	noPkts = fileSize/PACKET_SIZE + ((fileSize % PACKET_SIZE)?1:0) ;
 	fseek (fp, 0, SEEK_SET) ;
-
 	printf ("Size of file = %d\n", fileSize) ;
 	printf ("No of packets = %d\n", noPkts) ;
 
+    /* ------------------------------------------------------ */
+
+    int maxfd ;
 	relayEvenSock = setSockAddr (&relayEvenAddr, RELAY_EVEN_PORT) ;
 	relayOddSock = setSockAddr (&relayOddAddr, RELAY_ODD_PORT) ;
+    fd_set relayfds ;
+    FD_ZERO (&relayfds) ;
+    FD_SET (relayEvenSock, &relayfds) ;
+    FD_SET (relayOddSock, &relayfds) ;
+
+    maxfd = relayEvenSock > relayOddSock ? relayEvenSock : relayOddSock ;
+
+    /* ------------------------------------------------------ */
 
     int windowStart = 0, latestPkt = -1 , windowSize = WINDOW_SIZE ;
     int windowPktOffset[WINDOW_SIZE] ;
     int windowPktStat[WINDOW_SIZE] ;
-
-    for (i = 0 ; i < WINDOW_SIZE ; i++)
-    {
-        windowPktOffset[i] = -1 ;
-        windowPktStat[i] = 0 ;
-    }
+    initWindow (windowPktOffset, windowPktStat) ;
 
 	
     while (1)
@@ -94,15 +108,18 @@ int main(void)
         if (unsentInWindow(windowPktStat))
             sendInWindow (windowPktStat, windowPktOffset, datCache, windowStart, &latestPkt, noPkts, relayEvenSock, relayOddSock, &relayEvenAddr, &relayOddAddr, fp) ;
 
+        select (maxfd + 1 , &relayfds, NULL, NULL, NULL) ;
+        if (FD_ISSET (relayEvenSock))
+        {
+            ackPkt = recvfrom ()
+        }
+
         break ;
     }
 
     printf ("ALL STATUSES - \n") ;
     for (i = 0 ; i < WINDOW_SIZE ; i++)
-    {
-        //windowPktOffset[i] = -1 ;
         printf ("%d\n" ,windowPktStat[i]) ;
-    }
 
     	
 
